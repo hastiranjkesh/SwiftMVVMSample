91f9e3d9c6e9ca1fc7ce7bf04dbd30ab727eb7fd
hasti <hasti.ranjkesh@gmail.com>
2018-08-06 14:25:51 +0430

    bug fix

--- SwiftMVVMSample/Service/APIManager.swift
+++ SwiftMVVMSample/Service/APIManager.swift
@@ -52,7 +52,6 @@ class APIManager {
                     return
                 }
                 completionHandler(data)
-//                self.imageData.value = data
             })
             dataTask?.resume()
         }
